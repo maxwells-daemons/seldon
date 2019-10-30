d895f2a354c0f7e8e9c44a29ffb19975531c5a20
Whillikers <aidanswope@gmail.com>
2019-10-30 13:26:17 -0700

    Move backend declarations to pxd

---
+++ src/ccode/backend.pxd
@@ -0,0 +1,22 @@
+# cython: language_level=3, boundscheck=False, wraparound=False
+ctypedef unsigned long Bitboard
+
+cdef extern from "cbitboard.c":
+    cpdef unsigned int c_popcount_64(Bitboard x)
+    cpdef Bitboard c_resolve_move(Bitboard player, Bitboard opp,
+                                       Bitboard new_disk)
+    cpdef Bitboard c_make_singleton_bitboard(unsigned int x, unsigned int y)
+    cpdef Bitboard c_find_moves(Bitboard player, Bitboard opp)
+    cpdef Bitboard c_stability(Bitboard player, Bitboard opp)
+
+cdef extern from "csolver.h":
+    cdef struct _move:
+        int x
+        int y
+        int score
+    ctypedef _move move
+
+cdef extern from "csolver.c":
+    cdef move c_solve_game(Bitboard player, Bitboard opp)
+
+
--- src/ccode/backend.pyx
+++ src/ccode/backend.pyx
@@ -1,28 +1,8 @@
 # cython: language_level=3, boundscheck=False, wraparound=False
-
 import numpy as np
 cimport numpy as np
-
-ctypedef unsigned long Bitboard
-
-cdef extern from "cbitboard.c":
-    cpdef unsigned int c_popcount_64(Bitboard x)
-    cpdef Bitboard c_resolve_move(Bitboard player, Bitboard opp,
-                                       Bitboard new_disk)
-    cpdef Bitboard c_make_singleton_bitboard(unsigned int x, unsigned int y)
-    cpdef Bitboard c_find_moves(Bitboard player, Bitboard opp)
-    cpdef Bitboard c_stability(Bitboard player, Bitboard opp)
-
-cdef extern from "csolver.h":
-    cdef struct _move:
-        int x
-        int y
-        int score
-    ctypedef _move move
-
-cdef extern from "csolver.c":
-    cdef move c_solve_game(Bitboard player, Bitboard opp)
-
+from backend cimport (c_popcount_64, c_resolve_move, c_make_singleton_bitboard,
+                      c_find_moves, c_stability, _move, move, c_solve_game)
 
 # C ULL <-> ndarray interface
 cpdef Bitboard _serialize(np.ndarray arr):
