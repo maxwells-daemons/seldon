ade92aa2e31440078ccd570f7b52e2d25e42b561
Whillikers <aidanswope@gmail.com>
2019-10-30 13:30:43 -0700

    Various refactoring of game code

--- src/game.py
+++ src/game.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from enum import Enum, auto
 from string import ascii_lowercase
-from typing import Dict, NamedTuple, Type
+from typing import Dict, NamedTuple, Tuple, Type
 
 import numpy as np  # type: ignore
 
@@ -9,6 +9,16 @@ from backend import find_moves, resolve_move  # type: ignore
 
 BOARD_SIZE = 8
 BOARD_SHAPE = (BOARD_SIZE, BOARD_SIZE)
+
+
+class PlayerColor(Enum):
+    BLACK = "black"  # Values align with Board tuple names to allow getting pieces
+    WHITE = "white"
+
+    def opponent(self):
+        if self == PlayerColor.WHITE:
+            return PlayerColor.BLACK
+        return PlayerColor.WHITE
 
 
 class Move(NamedTuple):
@@ -26,6 +36,11 @@ class Board(NamedTuple):
     black: np.ndarray
     white: np.ndarray
 
+    def for_side(self, player: PlayerColor) -> Tuple[np.ndarray, np.ndarray]:
+        player_board = self.__getattribute__(player.value)
+        opponent_board = self.__getattribute__(player.opponent().value)
+        return player_board, opponent_board
+
     def _string_array(self) -> np.ndarray:
         board = np.tile(" ", (BOARD_SIZE + 1, BOARD_SIZE + 1))
         board[0, 1:] = [x for x in ascii_lowercase[:BOARD_SIZE]]
@@ -39,19 +54,9 @@ class Board(NamedTuple):
         return np.array2string(self._string_array(), formatter={"numpystr": str})
 
 
-class PlayerColor(Enum):
-    BLACK = "black"  # Values align with Board tuple names to allow getting pieces
-    WHITE = "white"
-
-    def opponent(self):
-        if self == PlayerColor.WHITE:
-            return PlayerColor.BLACK
-        return PlayerColor.WHITE
-
-
 class GameOutcome(Enum):
-    BLACK_WINS = auto()
-    WHITE_WINS = auto()
+    BLACK_WINS = "black"  # Values align with PlayerColor to compare
+    WHITE_WINS = "white"
     DRAW = auto()
 
 
@@ -71,7 +76,7 @@ class PlayerABC(ABC):
     """
 
     def __init__(self, color: PlayerColor) -> None:
-        self.color = color
+        self.color: PlayerColor = color
 
     @abstractmethod
     def get_move(self, player_board: np.ndarray, opponent_board: np.ndarray) -> Move:
@@ -107,6 +112,75 @@ def player_has_moves(player_board: Board, opp_board: Board) -> bool:
     return find_moves(player_board, opp_board).any()
 
 
+def winning_player(board: Board) -> GameOutcome:
+    black_score = np.count_nonzero(board.black)
+    white_score = np.count_nonzero(board.white)
+    if black_score > white_score:
+        return GameOutcome.BLACK_WINS
+    if white_score > black_score:
+        return GameOutcome.WHITE_WINS
+    return GameOutcome.DRAW
+
+
+def player_make_move(board: Board, move: Move, player: PlayerColor) -> Board:
+    player_board, opponent_board = board.for_side(player)
+    new_player_board, new_opponent_board = resolve_move(
+        player_board, opponent_board, move.x, move.y
+    )
+    return Board(
+        **{player.value: new_player_board, player.opponent().value: new_opponent_board}
+    )
+
+
+def play_game_from_state(
+    board: Board,
+    current_player: PlayerColor,
+    black: Type[PlayerABC],
+    white: Type[PlayerABC],
+) -> GameOutcome:
+    """
+    Play a game of Othello between two players, starting from a given state.
+
+    Parameters
+    ----------
+    board : Board
+        The starting board.
+    current_player : PlayerColor
+        The starting player.
+    black : PlayerABC constructor
+        Player class for the black player.
+    white : PlayerABC constructor
+        Player class for the white player.
+
+    Returns
+    -------
+    GameOutcome
+        The outcome of the game.
+    """
+
+    players: Dict[PlayerColor, PlayerABC] = {
+        PlayerColor.BLACK: black(PlayerColor.BLACK),
+        PlayerColor.WHITE: white(PlayerColor.WHITE),
+    }
+    just_passed: bool = False
+
+    while True:
+        player_board, opponent_board = board.for_side(current_player)
+
+        if not player_has_moves(player_board, opponent_board):
+            if just_passed:
+                break  # Both players pass: game ends
+            just_passed = True
+            current_player = current_player.opponent()
+            continue
+
+        move = players[current_player].get_move(player_board, opponent_board)
+        board = player_make_move(board, move, current_player)
+        current_player = current_player.opponent()
+
+    return winning_player(board)
+
+
 def play_game(black: Type[PlayerABC], white: Type[PlayerABC]) -> GameOutcome:
     """
     Play a complete game of Othello between two players.
@@ -123,42 +197,4 @@ def play_game(black: Type[PlayerABC], white: Type[PlayerABC]) -> GameOutcome:
     GameOutcome
         The outcome of the game.
     """
-    current_player: PlayerColor = PlayerColor.BLACK
-    players: Dict[PlayerColor, PlayerABC] = {
-        PlayerColor.BLACK: black(PlayerColor.BLACK),
-        PlayerColor.WHITE: white(PlayerColor.WHITE),
-    }
-    board: Board = starting_board()
-    just_passed: bool = False
-
-    while True:
-        player_board = board.__getattribute__(current_player.value)
-        opponent_board = board.__getattribute__(current_player.opponent().value)
-
-        if not player_has_moves(player_board, opponent_board):
-            if just_passed:
-                break  # Both players pass: game ends
-            just_passed = True
-            current_player = current_player.opponent()
-            continue
-
-        move = players[current_player].get_move(player_board, opponent_board)
-        new_player_board, new_opponent_board = resolve_move(
-            player_board, opponent_board, move.x, move.y
-        )
-        board = Board(
-            **{
-                current_player.value: new_player_board,
-                current_player.opponent().value: new_opponent_board,
-            }
-        )
-
-        current_player = current_player.opponent()
-
-    black_score = np.count_nonzero(board.black)
-    white_score = np.count_nonzero(board.white)
-    if black_score > white_score:
-        return GameOutcome.BLACK_WINS
-    if white_score > black_score:
-        return GameOutcome.WHITE_WINS
-    return GameOutcome.DRAW
+    return play_game_from_state(starting_board(), PlayerColor.BLACK, black, white)
