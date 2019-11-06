f6877bc5880a97debc45c2217fb8796531fb35f9
Whillikers <aidanswope@gmail.com>
2019-11-05 21:30:40 -0800

    Add model code for AlphaZero policy/value network

---
+++ src/players/alphazero/model.py
@@ -0,0 +1,103 @@
+import tensorflow as tf  # type: ignore
+import tensorflow.keras.layers as L  # type: ignore
+
+from board import BOARD_SHAPE, BOARD_SIZE, BOARD_SQUARES
+
+
+def residual_block(
+    x: tf.Tensor, filters: int, separable_convs: bool = False, prefix: str = ""
+) -> tf.Tensor:
+    """
+    Make a residual block of layers.
+
+    Parameters
+    ----------
+    x : tf.Tensor
+        The input tensor.
+    filters : int
+        How many filters to use for each convolution.
+    separable_convs : bool
+        Whether to use depthwise separable convolution.
+    prefix : str
+        How to prefix layer names within the block.
+
+    Returns
+    -------
+    tf.Tensor
+        The output tensor.
+    """
+
+    if separable_convs:
+        conv_fn = L.SeparableConv2D
+    else:
+        conv_fn = L.Conv2D
+
+    y = conv_fn(filters, (3, 3), padding="same", name=prefix + "conv2d_1")(x)
+    y = L.BatchNormalization(axis=-1, name=prefix + "batchnorm_1")(y)
+    y = L.ReLU(name=prefix + "relu_1")(y)
+    y = conv_fn(filters, (3, 3), padding="same", name=prefix + "conv2d_2")(y)
+    y = L.BatchNormalization(axis=-1, name=prefix + "batchnorm_2")(y)
+    y = L.add([x, y], name=prefix + "skip")
+    y = L.ReLU(name=prefix + "relu_2")(y)
+    return y
+
+
+def get_network(
+    filters: int,
+    blocks: int,
+    policy_head_filters: int = 2,
+    value_head_filters: int = 1,
+    value_dense_units: int = 64,
+    separable_convs: bool = False,
+) -> tf.keras.Model:
+    """
+    Make a (policy, value) network.
+
+    Parameters
+    ----------
+    filters : int
+        How many filters to use for the body convolutions.
+    blocks : int
+        How many residual blocks of depth to add.
+    policy_head_filters : int
+        How many filters to use for the initial policy head convolution.
+    value_head_filters : int
+        How many filters to use for the initial value head convolution.
+    value_dense_units : int
+        How many units to use for the value head's internal dense layer.
+    separable_convs : bool
+        If True, use depthwise-separable convolution.
+    """
+
+    if separable_convs:
+        conv_fn = L.SeparableConv2D
+    else:
+        conv_fn = L.Conv2D
+
+    inputs_ = L.Input(shape=(BOARD_SIZE, BOARD_SIZE, 2))
+    x = conv_fn(filters, (3, 3), padding="same", name="input_conv2d")(inputs_)
+    x = L.BatchNormalization(axis=-1, name="input_batchnorm")(x)
+    x = L.ReLU(name="input_relu")(x)
+
+    for block in range(blocks):
+        x = residual_block(x, filters, separable_convs, prefix=f"block_{block + 1}_")
+
+    # Policy head
+    policy = L.Conv2D(policy_head_filters, (1, 1), name="policy_conv2d")(x)
+    policy = L.BatchNormalization(axis=-1, name="policy_batchnorm")(policy)
+    policy = L.ReLU(name="policy_relu")(policy)
+    policy = L.Flatten(name="policy_flatten")(policy)
+    policy = L.Dense(BOARD_SQUARES, name="policy_dense")(policy)
+    policy = L.Reshape(BOARD_SHAPE, name="policy_unflatten")(policy)
+
+    # Value head
+    value = L.Conv2D(value_head_filters, (1, 1), name="value_conv2d")(x)
+    value = L.BatchNormalization(axis=-1, name="value_batchnorm")(value)
+    value = L.ReLU(name="value_relu_1")(value)
+    value = L.Flatten(name="value_flatten")(value)
+    value = L.Dense(value_dense_units, name="value_dense_1")(value)
+    value = L.ReLU(name="value_relu_2")(value)
+    value = L.Dense(1, name="value_dense_2")(value)
+    value = tf.keras.activations.tanh(value)
+
+    return tf.keras.Model(inputs=inputs_, outputs=(policy, value))
