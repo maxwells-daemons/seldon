1b23b96c112c4e3241a38a6215044431fa7a58f1
Whillikers <aidanswope@gmail.com>
2019-11-12 07:01:29 -0800

    Add player run script for alphazero

---
+++ src/player_configs/alphazero_network.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# cython: language_level=3, boundscheck=False, wraparound=False
+from typing import Tuple
+
+import click
+import numpy as np  # type: ignore
+import tensorflow as tf  # type: ignore
+
+import src.cs2_wrapper  # type: ignore
+import src.players.alphazero.alphazero_player  # type: ignore
+from board import BOARD_SHAPE
+
+
+@click.command()
+@click.option(
+    "--network",
+    type=click.Path(exists=True),
+    required=True,
+    help="Path to a saved Keras policy/value network.",
+)
+@click.option("--C", type=float, default=1.4, help="MCTS exploration parameter.")
+@click.option(
+    "--solver-depth",
+    type=int,
+    default=18,
+    help="Depth to run deterministic endgame solver from. If -1, do not use a solver.",
+)
+@click.option(
+    "--solver-time",
+    type=int,
+    default=5000,
+    help="Milliseconds to reserve for endgame solving.",
+)
+@click.option(
+    "--sims",
+    type=int,
+    default=100,
+    help="Number of simulations per turn if there is no time limit.",
+)
+@click.option(
+    "--buffer_time", type=int, default=80, help="Milliseconds to reserve each turn."
+)
+def main(
+    network: str,
+    C: float = 1.4,
+    solver_depth: int = 18,
+    solver_time: int = 5000,
+    sims: int = 100,
+    buffer_time: int = 80,
+):
+    model = tf.keras.models.load_model(network)
+
+    def network_evaluator(board: np.ndarray) -> Tuple[np.ndarray, float]:
+        policy, value = model.predict(np.expand_dims(board, axis=0).astype(np.float32))
+        policy = np.reshape(policy, BOARD_SHAPE)
+        return policy, value[0, 0]
+
+    player = src.players.alphazero.alphazero_player.AlphaZeroPlayer(
+        evaluator=network_evaluator, explore_coeff=C, finalized=True, sims_per_turn=sims
+    )
+
+    if solver_depth > 0:
+        player = player.with_depth_solver(solver_depth, solver_time)
+
+    src.cs2_wrapper.run_player(player)
+
+
+if __name__ == "__main__":
+    main()
